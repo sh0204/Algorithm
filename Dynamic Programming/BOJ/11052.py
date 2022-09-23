7a879de519ea283d3961885ddb8269eaa9b469cf
SOHEE <73006274+sh0204@users.noreply.github.com>
2022-09-23 04:49:03 +0000

    dp

---
+++ Dynamic Programming/BOJ/11052.py
@@ -0,0 +1,12 @@
+#11052 카드 구매하기
+
+#dp이용
+n = int(input())
+arr = [0] + list(map(int, input().split()))
+dp = [0 for _ in range(n + 1)]
+
+for i in range(1, n + 1):
+    for j in range(1, i + 1):
+        dp[i] = max(dp[i], dp[i - j] + arr[j])
+
+print(dp[i])
