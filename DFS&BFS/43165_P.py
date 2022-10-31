70f6e7ade4dee78cce69df9ae1a313248951de52
SOHEE <73006274+sh0204@users.noreply.github.com>
2022-10-31 10:46:23 +0000

    dfs

---
+++ DFS&BFS/43165_P.py
@@ -0,0 +1,19 @@
+# 타겟 넘버
+# 프로그래머스
+# DFS/BFS
+
+
+def solution(numbers, target):
+    n = len(numbers)
+    answer = 0
+    def dfs(idx, result):
+        if idx == n:
+            if result == target:
+                nonlocal answer
+                answer+=1
+            return 
+        else:
+            dfs(idx+1, result+numbers[idx])
+            dfs(idx+1, result-numbers[idx])
+    dfs(0,0)
+    return answer
